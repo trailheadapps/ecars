efc1dd4f2b36a1da66d360645c8d615b933f8a9d
Julian Duque <julianduquej@gmail.com>
2020-12-16 16:10:43 -0500

    use arrow function on onmessage

--- force-app/main/default/lwc/liveData/liveData.js
+++ force-app/main/default/lwc/liveData/liveData.js
@@ -27,29 +27,28 @@ export default class LiveData extends LightningElement {
                 const ctx = canvas.getContext('2d');
                 this.chart = new window.Chart(ctx, this.chartJsConfig);
                 const ws = new window.WebSocket('wss://example.herokuapp.com');
-                const that = this;
                 // For the simplicity of this demo we are just listening to the
                 // first car that sends its telemetry data.
                 //
                 // In an upcoming iteration of eCars this will be enhanced by
                 // specifying a car from Postgres in combination with
-                ws.onmessage = function (event) {
-                    if (!that._updateChart) return;
+                ws.onmessage = (event) => {
+                    if (!this._updateChart) return;
                     const wsElement = JSON.parse(JSON.parse(event.data));
-                    if (!that.firstCar) that.firstCar = wsElement.name;
-                    if (wsElement.name === that.firstCar) {
-                        that.wsData.push(wsElement);
-                        that.chart.data.datasets[0].data.push(wsElement.mpge);
-                        that.chart.data.datasets[1].data.push(
+                    if (!this.firstCar) this.firstCar = wsElement.name;
+                    if (wsElement.name === this.firstCar) {
+                        this.wsData.push(wsElement);
+                        this.chart.data.datasets[0].data.push(wsElement.mpge);
+                        this.chart.data.datasets[1].data.push(
                             wsElement.battery
                         );
-                        that.chart.data.datasets[2].data.push(wsElement.range);
-                        if (that.chart.data.datasets[0].data.length > 10) {
-                            that.chart.data.datasets[0].data.splice(0, 1);
-                            that.chart.data.datasets[1].data.splice(0, 1);
-                            that.chart.data.datasets[2].data.splice(0, 1);
+                        this.chart.data.datasets[2].data.push(wsElement.range);
+                        if (this.chart.data.datasets[0].data.length > 10) {
+                            this.chart.data.datasets[0].data.splice(0, 1);
+                            this.chart.data.datasets[1].data.splice(0, 1);
+                            this.chart.data.datasets[2].data.splice(0, 1);
                         }
-                        that.chart.update();
+                        this.chart.update();
                     }
                 };
             })
