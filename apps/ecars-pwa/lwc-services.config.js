d566c7ba4cd0310ea7d2240016668e4e31b880fd
Julián Duque <julianduquej@gmail.com>
2021-07-08 15:46:47 -0400

    Update Dependencies and package-lock
    * update dependencies & lockfiles
    
    * downgrade wrap-ansi to v7
    
    * fonts were removed from slds
    
    * update ecars-pwa with latest lwc-services
    
    * remove Salesforce Fonts

--- apps/ecars-pwa/lwc-services.config.js
+++ apps/ecars-pwa/lwc-services.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     resources: [
         { from: 'src/client/resources', to: 'dist/' },
-        { from: 'src/client/index.html', to: 'dist/index.html' },
         { from: 'src/client/manifest.json', to: 'dist/manifest.json' }
     ],
     sourceDir: './src/client'
--- apps/ecars-pwa/scripts/webpack.config.js
+++ apps/ecars-pwa/scripts/webpack.config.js
@@ -1,9 +1,15 @@
 // Custom webpack configuration file, provides generation of service worker
 // More information: https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin
 const { InjectManifest } = require('workbox-webpack-plugin');
+const path = require('path');
 
 module.exports = {
     plugins: [
         new InjectManifest({ swSrc: './src/client/sw.js', swDest: 'sw.js' })
-    ]
+    ],
+    entry: './src/client/index.js',
+    output: {
+        path: path.join(__dirname, '..', 'dist'),
+        filename: '[name].js'
+    }
 };
