43cca5f866d5bb839d0155765a3915aca305745b
adityanaag3 <atopalli@salesforce.com>
2022-03-22 17:26:11 +0530

    Minor Tweaks

--- force-app/main/default/lwc/leadSummary/leadSummary.js
+++ force-app/main/default/lwc/leadSummary/leadSummary.js
@@ -1,4 +1,5 @@
 import { LightningElement, api, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 
 import LEAD_FIELD from '@salesforce/schema/Car_Configuration__c.Lead__c';
@@ -8,7 +9,7 @@ import MOBILE_FIELD from '@salesforce/schema/Lead.MobilePhone';
 import PHONE_FIELD from '@salesforce/schema/Lead.Phone';
 const fields = [LEAD_FIELD];
 
-export default class LeadSummary extends LightningElement {
+export default class LeadSummary extends NavigationMixin(LightningElement) {
     @api recordId;
 
     leadFields = [NAME_FIELD, EMAIL_FIELD, PHONE_FIELD, MOBILE_FIELD];
@@ -18,5 +19,16 @@ export default class LeadSummary extends LightningElement {
 
     get lead() {
         return getFieldValue(this.configuration.data, LEAD_FIELD);
+    }
+
+    gotoDetail(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                actionName: "view",
+                recordId: this.lead,
+                objectApiName: "Lead"
+            }
+        });
     }
 }
