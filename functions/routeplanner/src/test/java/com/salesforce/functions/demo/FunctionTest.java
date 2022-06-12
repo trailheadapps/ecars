cb478cc8f02d88609c9be97ff5fa805ccd3db85e
Mohith Shrivastava <mohith.shrivastava@salesforce.com>
2022-06-12 00:56:04 -0400

    feat: fix test class

---
+++ functions/routeplanner/src/test/java/com/salesforce/functions/demo/FunctionTest.java
@@ -0,0 +1,13 @@
+package com.salesforce.functions.demo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class FunctionTest {
+
+  @Test
+  public void test() throws Exception {
+    assertEquals(true, true);
+  }
+}
